ab892f590405ae680720f28da45f77000094e5d5
Demetris Perdikos <perdikos.jimmy@gmail.com>
2022-03-02 20:32:33 -0500

    RA-1 simple project

---
+++ work.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    string first, last; 
+    
+    cout<<"Welcome to CS 280." << endl;
+    
+    cout<<"What is your first and last names?" << endl;
+    
+    cin >> first >> last;
+    
+    cout << "Hello, " + first + " " + last << endl; 
+    
+    return 0;
+}
